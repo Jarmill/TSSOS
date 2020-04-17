1f012dee8dff360fe559a1c541850cbaa8c9371b
Jared Miller <jared.fr.miller@gmail.com>
2020-04-17 00:54:50 -0400

    Basis with binary variables and modified multiplication.
    Will start to integrate this into the core code tomorrow (fblocks, gblocks, basis, everywhere).
    
    May also try to reconstruct Jie's basis generation procedure, and do this with binary variables. This would remove the indexing I perform in basis_out.

--- example/basis_test.jl
+++ example/basis_test.jl
@@ -1,5 +1,11 @@
 using TSSOS
-using DataFrames
+
+function bin_add(bi, bj, n_binary)
+    "multiply two monomials together, where the first n_binary variables are binary"
+    bs = bi + bj
+    bs[1:n_binary] = any.(x->x>0 , bs[1:n_binary])
+    return bs
+end
 
 n = 4
 nb= 2
@@ -12,4 +18,8 @@ basis_bin = basis[1:nb, :]
 basis_valid = all.(x-> x<=1, Base.eachcol(basis_bin))
 basis_out = basis[:, basis_valid]
 
-e
+bi = basis_out[:, 6]
+bj = basis_out[:, 37]
+bs = bi + bj
+
+bnew = bin_add(bi, bj, nb)
--- example/example_limited.jl
+++ example/example_limited.jl
@@ -8,4 +8,6 @@ g_1=2-x[1]^2-2*x[2]^2
 g_2=x[1]^2-x[2]^2-1
 pop=[f,g_1,g_2]
 d=2 # the order of Lasserre's hierarchy
+
+# opt,sol,data= blockcpop_first(pop,x,d,numeq=1,solve=true,solution=false)
 opt,sol,data= blockcpop_first(pop,x,d,numeq=1,solve=true,solution=true)
